9360a4144234000e791cd9264c6633a22a55fb2e
Joerg Henrichs <henrichsjoerg@gmail.com>
2024-12-03 13:56:09 +1100

    Use new shell tool and linker inheritance.

--- infrastructure/build/fab/default/setup_gnu.py
+++ infrastructure/build/fab/default/setup_gnu.py
@@ -7,19 +7,7 @@ This function gets called from the default site-specific config file
 '''
 
 from fab.build_config import BuildConfig
-from fab.tools import Category, Tool, ToolRepository
-
-
-class Shell(Tool):
-    '''A simple wrapper that runs a shell script.
-    :name: the path to the script to run.
-    '''
-    def __init__(self, name: str):
-        super().__init__(name=name, exec_name=name,
-                         category=Category.MISC)
-
-    def check_available(self):
-        return True
+from fab.tools import Category, ToolRepository
 
 
 def setup_gnu(build_config: BuildConfig):
@@ -38,24 +26,22 @@ def setup_gnu(build_config: BuildConfig):
              ]
     gfortran.add_flags(flags)
 
-    # ATM a linker is not using a compiler wrapper, and so
-    # linker-mpif90-gfortran does not inherit from linker-gfortran.
-    # For now set the flags in both linkers:
-    bash = Shell("bash")
+    # This will implicitly affect all gfortran based linkers, e.g.
+    # linker-mpif90-gfortran will use these flags as well.
+    linker = tr.get_tool(Category.LINKER, "linker-gfortran")
+    # ATM we don't use a shell when running a tool, and as such
+    # we can't directly use "$()" as parameter. So query these values using
+    # Fab's shell tool (doesn't really matter which shell we get, so just
+    # ask for the default):
+    shell = tr.get_default(Category.SHELL)
     # We must remove the trailing new line, and create a list:
-    nc_flibs = bash.run(additional_parameters=["-c", "nf-config --flibs"],
-                        capture_output=True).strip().split()
+    nc_flibs = shell.run(additional_parameters=["-c", "nf-config --flibs"],
+                         capture_output=True).strip().split()
 
-    for linker_name in ["linker-gfortran", "linker-mpif90-gfortran"]:
-        linker = tr.get_tool(Category.LINKER, linker_name)
-        # ATM we don't use a shell when starting a tool, and as such
-        # we can't directly use "$()". So query these values using
-        # a dummy shell tool:
+    linker.add_lib_flags("netcdf", nc_flibs, silent_replace=True)
+    linker.add_lib_flags("yaxt", ["-lyaxt", "-lyaxt_c"])
+    linker.add_lib_flags("xios", ["-lxios"])
+    linker.add_lib_flags("hdf5", ["-lhdf5"])
 
-        linker.add_lib_flags("netcdf", nc_flibs, silent_replace=True)
-        linker.add_lib_flags("yaxt", ["-lyaxt", "-lyaxt_c"])
-        linker.add_lib_flags("xios", ["-lxios"])
-        linker.add_lib_flags("hdf5", ["-lhdf5"])
-
-        # Always link with C++ libs
-        linker.add_post_lib_flags(["-lstdc++"])
+    # Always link with C++ libs
+    linker.add_post_lib_flags(["-lstdc++"])
