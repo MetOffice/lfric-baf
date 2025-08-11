bbaa59d412e225333902095c1e6350268d5343f2
Joerg Henrichs <henrichsjoerg@gmail.com>
2025-08-11 10:04:10 +1000

    #75 Minox fixes in compiler setup files.

--- infrastructure/build/fab/fcm_extract.py
+++ infrastructure/build/fab/fcm_extract.py
@@ -4,10 +4,12 @@
 This module contains a class that reads in fcm extract specifications.
 '''
 
-
+import logging
 from pathlib import Path
 import re
 import sys
+
+logger = logging.getLogger(__name__)
 
 
 class FcmExtract(dict):
@@ -15,22 +17,22 @@ class FcmExtract(dict):
     A simple class that reads in an fcm extract.cfg file and stores
     the information about excluded and included file to be used in FAB.
 
-    :param str filename: the name of the fcm extract file to read.
+    :param filename: the name of the fcm extract file to read.
     '''
 
-    def __init__(self, filename):
+    def __init__(self, filename: Path) -> None:
         # pylint: disable=too-many-branches, too-many-statements
         # pylint: disable=too-many-locals
         super().__init__()
         re_comment = re.compile(r"( *#.*$)")
         re_include = re.compile(r"^ *include", re.I)
-        re_location = re.compile(r"^ *extract.location\{diff\}\[.*\] *=")
+        re_location = re.compile(r"^ *extract.location(\{.*\})?\[.*\] *=")
         re_files = re.compile(r"^ *(.*)_extract_files.* *= *(.*) *$")
         re_excl = re.compile(r"^ *extract\.path-excl\[(.*)\] *= *(.*) *$")
         re_incl = re.compile(r"^ *extract\.path-incl\[(.*)\] *= *(.*) *$")
 
         # Read the files, remove comments and empty lines, and handle '\'
-        with open(filename, encoding="utf8") as f_in:
+        with filename.open(mode="r", encoding="utf8") as f_in:
             current_line = []
             for line in f_in:
                 line = line.strip()
@@ -48,9 +50,9 @@ class FcmExtract(dict):
                 current_line = []
                 if not line:
                     continue
-                if not line or re_include.match(line):
-                    # Includes are not supported
-                    print(f"Ignoring include '{line}'.")
+                if re_include.match(line):
+                    # Including other files are not supported
+                    logger.warning(f"Ignoring include '{line}'.")
                     continue
                 if re_location.match(line):
                     # Ignore location info
@@ -69,10 +71,10 @@ class FcmExtract(dict):
                     line_type = "exclude"
                 else:
                     grp = re_incl.match(line)
+                    if not grp:
+                        logger.warning(f"Unexpected line: '{line}' - ignored.")
+                        continue
                     line_type = "include"
-                if not grp:
-                    print(f"Unexpected line: '{line}' - ignored.")
-                    continue
                 section = grp.group(1).lower()
                 list_of_paths = [Path(i) for i in grp.group(2).split(" ")]
                 if section in self:
@@ -86,7 +88,7 @@ def main():
     '''
     Simple wrapper to avoid pylint errors.
     '''
-    fe = FcmExtract(sys.argv[1])
+    fe = FcmExtract(Path(sys.argv[1]))
     print("Sections", fe.keys())
     for section, list_of_paths in fe.items():
         print("SECTION:", section, list_of_paths)
