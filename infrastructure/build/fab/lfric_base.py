f554af4fea4ec9dc79ea83c21fc58b388eafb9a5
Junwei Lyu <jl8309@gadi-login-05.gadi.nci.org.au>
2025-08-18 18:50:18 +1000

    Modified analyse_step ignore_mod_deps to cover None scenario

--- infrastructure/build/fab/lfric_base.py
+++ infrastructure/build/fab/lfric_base.py
@@ -366,11 +366,12 @@ class LFRicBase(FabBase):
         '''
         self.preprocess_x90_step()
         self.psyclone_step()
-        lfric_general_ignore_mod_deps=['netcdf', 'MPI', 'yaxt',
-                                'pfunit_mod', 'xios', 'mod_wait']
+        if ignore_mod_deps is None:
+            ignore_mod_deps = []
+        ignore_mod_deps += ['netcdf', 'MPI', 'yaxt',
+                            'pfunit_mod', 'xios', 'mod_wait']
         analyse(self.config, root_symbol=self.root_symbol,
-                ignore_mod_deps=(ignore_mod_deps+
-                lfric_general_ignore_mod_deps))
+                ignore_mod_deps=ignore_mod_deps)
 
     def preprocess_x90_step(self) -> None:
         """
