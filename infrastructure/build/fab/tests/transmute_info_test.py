42d538c3d18bbc76b4fab13c5fba8a1140d70c5a
Joerg Henrichs <henrichsjoerg@gmail.com>
2026-02-18 13:39:33 +1100

    Improved support for reading transmutation info.

--- infrastructure/build/fab/tests/transmute_info_test.py
+++ infrastructure/build/fab/tests/transmute_info_test.py
@@ -24,65 +24,98 @@ def sample_makefile(tmp_path: Path) -> Path:
     """
     content = """\
 # Comment
-export PSYCLONE_PHYSICS_FILES = a b c \\
-                                d \\
-                                e
+export PSYCLONE_PHYSICS_FILES = a1 b1 c1 \\
+                                d1 \\
+                                e1
 export PSYCLONE_DIRECTORIES = science/ukca
 
-# Empty list:
-
-export PSYCLONE_PHYSICS_EXCEPTION =
+export PSYCLONE_PHYSICS_EXCEPTION = /some/science/ukca/exception.f90
 """
     file_path = tmp_path / "psyclone_transmute_file_list.mk"
     file_path.write_text(content, encoding="utf8")
     return file_path
 
 
-def test_get_revision_parses_revisions_correctly(tmp_path):
+def test_parse_makefile(sample_makefile):
     """
-    Tests the parsing of a dependencies.sh file
+    Tests the parsing of a makefile
+    """
+
+    ti = TransmuteInfo()
+    ti.import_makefile(sample_makefile, mode="include")
+    for i in ["a1", "b1", "c1", "d1", "e1"]:
+        assert (i, f"{i}.py") in ti._pattern_to_script
+    assert ("science/ukca", "local.py") not in ti._pattern_to_script
+
+    ti.import_makefile(sample_makefile, mode="exclude")
+    assert ("science/ukca", "local.py") in ti._pattern_to_script
+    assert ("/some/science/ukca/exception.f90", "") in ti._pattern_to_script
+
+
+def test_exceptions(tmp_path):
+    """
+    Test invalid info in the parsed makefile
     """
     content = """\
 # Comment
-export PSYCLONE_PHYSICS_FILES = a b c \\
-                                d \\
-                                e
+export INVALID = a b c
 export PSYCLONE_DIRECTORIES = science/ukca
-
-# Empty list:
-
 export PSYCLONE_PHYSICS_EXCEPTION =
 """
     makefile = tmp_path / "psyclone_transmute_file_list.mk"
     makefile.write_text(content, encoding="utf8")
 
     ti = TransmuteInfo()
-    ti.import_makefile(makefile)
-    assert ti.get_transmute_files() == ["a", "b", "c", "d", "e"]
-    assert ti.get_transmute_directories() == ["science/ukca"]
-    assert ti.get_transmute_exceptions() == []
+    with pytest.raises(ValueError) as err:
+        ti.import_makefile(makefile, mode="include")
+
+    assert "Unexpected line 'export INVALID = a b c' in file" in str(err.value)
+
+    with pytest.raises(ValueError) as err:
+        ti.import_makefile(makefile, mode="INVALID")
+
+    assert ("expects 'include' or 'exclude' as mode, but got 'invalid'"
+            in str(err.value))
 
 
-def test_get_revision_parses_revisions_correctly_error(tmp_path):
+def test_get_file_list(sample_makefile):
     """
-    Tests the parsing of a dependencies.sh file
+    Tests querying the script information
     """
-    content = """\
-# Comment
-export X_PSYCLONE_PHYSICS_FILES = a b c \\
-                                d \\
-                                e
-export PSYCLONE_DIRECTORIES = science/ukca
+    ti = TransmuteInfo()
+    ti.import_makefile(sample_makefile, mode="include")
 
-# Empty list:
+    # Test a single file:
+    files = ti.get_transmute_files(set([Path("/somewhere/a1.f90")]))
+    assert files == set([(Path("/somewhere/a1.f90"), "a1.py")])
 
-export PSYCLONE_PHYSICS_EXCEPTION =
-"""
-    makefile = tmp_path / "psyclone_transmute_file_list.mk"
-    makefile.write_text(content, encoding="utf8")
+    # Test a file and a file in the directory
+    files = ti.get_transmute_files(set(
+        [Path("/somewhere/b1.f90"),
+         Path("/some/science/ukca/where/t.f90")]))
+    # t.f90 should not be listed (since it was read in 'include' mode)
+    assert files == set([(Path("/somewhere/b1.f90"), "b1.py")])
+
+    # Test a file, directory and an exception
+    files = ti.get_transmute_files(set(
+        [Path("/somewhere/b1.f90"),
+         Path("/some/science/ukca/exception.f90"),
+         Path("/some/science/ukca/where/t.f90")]))
+    assert files == set(
+        [(Path("/somewhere/b1.f90"), "b1.py")])
 
     ti = TransmuteInfo()
-    with pytest.raises(RuntimeError) as err:
-        ti.import_makefile(makefile)
+    ti.import_makefile(sample_makefile, mode="exclude")
+    files = ti.get_transmute_files(set(
+        [Path("/somewhere/b1.f90"),
+         Path("/some/science/ukca/where/t.f90")]))
+    # t.f90 should not be listed (since it was read in 'include' mode)
+    assert files == set([(Path("/some/science/ukca/where/t.f90"), "local.py")])
 
-    assert "Section 'PSYCLONE_PHYSICS_FILES' not found in" in str(err.value)
+    # Test a file, directory and an exception
+    files = ti.get_transmute_files(set(
+        [Path("/somewhere/b1.f90"),
+         Path("/some/science/ukca/exception.f90"),
+         Path("/some/science/ukca/where/t.f90")]))
+    assert files == set(
+        [(Path("/some/science/ukca/where/t.f90"), "local.py")])
--- infrastructure/build/fab/transmute_info.py
+++ infrastructure/build/fab/transmute_info.py
@@ -12,13 +12,19 @@ This is expected to be replaced with a yaml file.
 """
 
 
+from pathlib import Path
 import re
+from typing import Union
 
 
 class TransmuteInfo:
     """
     This class manages the information about which files to
-    transmute.
+    transmute. It manages a (sorted) list of tuples: (pattern, script)
+    to indicate which pattern will trigger the use of which PSyclone
+    script. The 'pattern' is checked using a simple substring operation,
+    e.g. "science/ukca"
+
     For now, it reads the content of psyclone_transmute_file_list.mk
     to get the required data.
     TODO: change this to a yaml file.
@@ -28,15 +34,14 @@ class TransmuteInfo:
                     "PSYCLONE_DIRECTORIES",
                     "PSYCLONE_PHYSICS_EXCEPTION"]
 
-    # The indices of the section in ALL_SECTIONS:
-    PHYSICS_FILES = 0
-    DIRECTORIES = 1
-    EXCEPTIONS = 2
-
     def __init__(self):
         self._vars = {}
+        # A list of ('pattern', 'script') pairs, indicating
+        # which PSyclone script to use for which pattern, whicn
+        # can be a filename, directories, ...
+        self._pattern_to_script: list[tuple(str, str)] = []
 
-    def import_makefile(self, filename: str) -> None:
+    def import_makefile(self, filename: str, mode: str) -> None:
         """
         This method reads in a makefile which defines variables, e.g.
             export PSYCLONE_PHYSICS_FILES = mphys_kernel_mod \
@@ -44,11 +49,21 @@ class TransmuteInfo:
             export PSYCLONE_DIRECTORIES = science/ukca
 
         This method is to support the original makefile design, and
-        can be removed later.
+        can be removed later. While the original design supports an
+        exclude and include mode (to select which part of the
+        makefile to use: files or directories/exceptions), this
+        class implements a more flexible way of determining the
+        script to use for which file.
 
-        :param: filename to read
+        :param filename: filename to read.
+        :param mode: 'include' or 'exclude' to describe which section to
+            extract from the Makefile.
         """
 
+        mode = mode.lower()
+        if mode not in ["exclude", "include"]:
+            raise ValueError(f"TransmuteInfo.import_makefile expects 'include'"
+                             f" or 'exclude' as mode, but got '{mode}'.")
         all_lines = []
 
         # First, read the makefile, and handle continuation markers
@@ -72,34 +87,63 @@ class TransmuteInfo:
                     current_line = ""
 
         re_export = re.compile("^ *export *([a-zA-Z0-9_]+) *= *(.*$)")
+
         for line in all_lines:
             grps = re_export.match(line)
-            if grps:
-                self._vars[grps[1]] = grps[2].split()
+            if not grps:
+                # Ignore comments and empty lines
+                continue
 
-        for section in TransmuteInfo.ALL_SECTIONS:
-            if section not in self._vars:
-                raise RuntimeError(f"Section '{section}' not found "
-                                   f"in '{filename}'.")
+            if grps[1].upper() == "PSYCLONE_PHYSICS_FILES":
+                # Only read in include mode:
+                if mode == "include":
+                    for pattern in grps[2].split():
+                        self._pattern_to_script.append((pattern,
+                                                        f"{pattern}.py"))
+                continue
 
-    def get_transmute_files(self) -> list[str]:
+            if grps[1].upper() == "PSYCLONE_PHYSICS_EXCEPTION":
+                # Only read in exclude mode:
+                if mode == "exclude":
+                    # Exception: set 'no script'
+                    for pattern in grps[2].split():
+                        self._pattern_to_script.append((pattern, ""))
+                continue
+
+            if grps[1].upper() == "PSYCLONE_DIRECTORIES":
+                # Only read in exclude mode:
+                if mode == "exclude":
+                    for pattern in grps[2].split():
+                        # This seems to be not well defined in current LFRic :(
+                        self._pattern_to_script.append((pattern, "local.py"))
+                continue
+
+            raise ValueError(f"Unexpected line '{line}' in file"
+                             f"'{filename}'.")
+
+    def get_transmute_files(self,
+                            file_set: set[Path]) -> set[Union[Path, str]]:
         """
+        Filters the specified list of paths for all files that have
+        a transmutation script assigned. It returns a set of tuples:
+        first the path, then the script to use. If a path matches
+        several pattern in a file, the last one specified will be
+        returned.
+
+        :param file_set: set of files to filter and determining the
+            script to use for.
+
         :returns: the list of physics files which should be transmuted.
-        """
-        return self._vars[
-            TransmuteInfo.ALL_SECTIONS[TransmuteInfo.PHYSICS_FILES]]
 
-    def get_transmute_directories(self) -> list[str]:
         """
-        :returns: the list of directories in which all files must be
-            transmuted.
-        """
-        return self._vars[
-            TransmuteInfo.ALL_SECTIONS[TransmuteInfo.DIRECTORIES]]
-
-    def get_transmute_exceptions(self) -> list[str]:
-        """
-        :returns: the list of file exceptions from the directories.
-        """
-        return self._vars[
-            TransmuteInfo.ALL_SECTIONS[TransmuteInfo.EXCEPTIONS]]
+        result: set[tuple[Path, str]] = set()
+        for filename in file_set:
+            name_str = str(filename)
+            final_script = ""
+            for pattern, script in self._pattern_to_script:
+                if pattern in name_str:
+                    final_script = script
+            print("XX", filename, final_script, self._pattern_to_script)
+            if final_script:
+                result.add((filename, final_script))
+        return result
